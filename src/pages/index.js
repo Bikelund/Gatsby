e94d1d4ff70366dddc633aef0411d8ad5980aaad
Bikelund <nicklas_barklund@hotmail.com>
2019-08-27 11:15:27 +0200

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: `blue` }}>
+    <Link to="/contact/">Contact</Link>
+    <h1>Hello there!</h1>
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="Random" />
+  </div>
+)
